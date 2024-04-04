d48ad8f21372575cd598f96e0eb4c58604ff794b
David <dfl8@aluno.ifal.edu.br>
2024-04-04 01:00:47 -0300

    feat: add green to MyColors class

--- lib/styles/global.dart
+++ lib/styles/global.dart
@@ -6,4 +6,5 @@ class MyColors {
   static const Color unimatchSemiBlack = Color(0xFF27272A);
   static const Color unimatchGray = Color(0xFF4B5563);
   static const Color unimatchWhite = Color(0xFFFAFAFA);
+  static const Color unimatchGreen = Color(0xFF40F99B);
 }
